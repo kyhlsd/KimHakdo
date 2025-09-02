a7fc794e3e1a01533e1c4436ad912f8ea630870f
kyhlsd <kmyghn@gmail.com>
2025-09-02 18:28:33 +0900

    Feat: 색상, 이미지 에셋, 앱 폰트, 여백, CornerRadius 세팅

---
+++ KimHakdo/Common/AppDesign/AppFont.swift
@@ -0,0 +1,15 @@
+//
+//  AppFont.swift
+//  KimHakdo
+//
+//  Created by 김영훈 on 9/2/25.
+//
+
+import UIKit
+
+enum AppFont {
+    static let title = UIFont.systemFont(ofSize: 28, weight: .heavy)
+    static let subtitle = UIFont.systemFont(ofSize: 14, weight: .bold)
+    static let body = UIFont.systemFont(ofSize: 14, weight: .regular)
+    static let caption = UIFont.systemFont(ofSize: 12, weight: .regular)
+}
