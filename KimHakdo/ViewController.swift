7f68fd982d6f3485f4b0fc46e3245831b9ce5e3c
kyhlsd <kmyghn@gmail.com>
2025-09-02 17:54:00 +0900

    Initial Commit

---
+++ KimHakdo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KimHakdo
+//
+//  Created by 김영훈 on 9/2/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
